496a286b1945a1465085ca8841a03d10d85d6dc8
Andy Wright <atwright147@gmail.com>
2019-04-04 23:03:30 +0100

    add testing framework and initial test

---
+++ src/converter.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { Converter } from './converter';
+
+describe('Converter', () => {
+  let convertInstance;
+
+  beforeEach(() => {
+    convertInstance = new Converter(100, 'cm', 'meter');
+  });
+
+  it('convert 100 cm to 0.1 meters', () => {
+    expect(convertInstance.convert()).to.equal(0.1);
+  });
+});
