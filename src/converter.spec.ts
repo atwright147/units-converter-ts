310087b37383a56b0c301cdb925fe02d31509d43
Andy Wright <atwright147@gmail.com>
2019-04-05 09:53:13 +0100

    fix incorrectly defined test

--- src/converter.spec.ts
+++ src/converter.spec.ts
@@ -6,10 +6,10 @@ describe('Converter', () => {
   let convertInstance;
 
   beforeEach(() => {
-    convertInstance = new Converter(100, 'cm', 'meter');
+    convertInstance = new Converter(10, 'cm', 'meter');
   });
 
-  it('convert 100 cm to 0.1 meters', () => {
+  it('convert 10 cm to 0.1 meters', () => {
     expect(convertInstance.convert()).to.equal(0.1);
   });
 });
