e55cc2932c6f7e6a87cc469d7481e7e1167b0f7d
Andy Wright <atwright147@gmail.com>
2019-04-10 17:20:39 +0100

    make the user interface work

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
 
+/**
+ * @example uconv <conversionType> <sourceSize> <sourceUnit> <destUnit>
+ */
+
 import { LengthConverter } from './converters/length-converter';
 
-const converter = new LengthConverter(100, 'cm', 'inch');
+const args = process.argv
+  .splice(2, process.argv.length)
+  .map(item => item.toLowerCase());
+
+const conversionType = args[0];
+const sourceSize = Number(args[1]);
+const sourceUnit = args[2];
+const destUnit = args[3];
+
+let converter;
+
+switch (conversionType) {
+  case 'length':
+    converter = new LengthConverter(sourceSize, sourceUnit, destUnit);
+    break;
+
+  default:
+    break;
+}
+
 
 console.info(converter.convert());
